db9b3da7f34646b9ecbf2ff015008af1def30f5d
kienct <kienct@vnpay.vn>
2022-03-22 01:46:44 +0700

    Initial commit

---
+++ app/src/main/java/com/frsarker/todotask/MainActivity.java
@@ -0,0 +1,174 @@
+package com.frsarker.todotask;
+
+import android.content.Intent;
+import android.database.Cursor;
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.widget.NestedScrollView;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class MainActivity extends AppCompatActivity {
+
+    DBHelper mydb;
+    LinearLayout empty;
+    NestedScrollView scrollView;
+    EditText etSearch;
+    TextView tvNoResults;
+    LinearLayout todayContainer, tomorrowContainer, otherContainer;
+    NoScrollListView taskListToday, taskListTomorrow, taskListUpcoming;
+    ArrayList<HashMap<String, String>> todayList = new ArrayList<HashMap<String, String>>();
+    ArrayList<HashMap<String, String>> tomorrowList = new ArrayList<HashMap<String, String>>();
+    ArrayList<HashMap<String, String>> upcomingList = new ArrayList<HashMap<String, String>>();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
+        setContentView(R.layout.activity_main);
+
+        mydb = new DBHelper(this);
+        empty = findViewById(R.id.empty);
+        scrollView = findViewById(R.id.scrollView);
+        todayContainer = findViewById(R.id.todayContainer);
+        tomorrowContainer = findViewById(R.id.tomorrowContainer);
+        otherContainer = findViewById(R.id.otherContainer);
+        taskListToday = findViewById(R.id.taskListToday);
+        taskListTomorrow = findViewById(R.id.taskListTomorrow);
+        taskListUpcoming = findViewById(R.id.taskListUpcoming);
+        etSearch = findViewById(R.id.etSearch);
+        tvNoResults = findViewById(R.id.tvNoResults);
+
+        //vừa nhập vừa search
+        etSearch.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                fetchDataFromDB(charSequence.toString());
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
+    }
+
+    public void openAddModifyTask(View view) {
+        startActivity(new Intent(this, AddModifyTask.class));
+    }
+
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        populateData();
+    }
+
+
+    public void populateData() {
+        mydb = new DBHelper(this);
+
+        runOnUiThread(new Runnable() {
+            public void run() {
+                fetchDataFromDB("");
+            }
+        });
+    }
+
+
+    public void fetchDataFromDB(String search) {
+        todayList.clear();
+        tomorrowList.clear();
+        upcomingList.clear();
+
+        Cursor today = mydb.getTodayTask(search);
+        Cursor tomorrow = mydb.getTomorrowTask(search);
+        Cursor upcoming = mydb.getUpcomingTask(search);
+
+        loadDataList(today, todayList);
+        loadDataList(tomorrow, tomorrowList);
+        loadDataList(upcoming, upcomingList);
+
+
+        if (todayList.isEmpty() && tomorrowList.isEmpty() && upcomingList.isEmpty() && etSearch.getVisibility() != View.VISIBLE) {
+            empty.setVisibility(View.VISIBLE);
+            scrollView.setVisibility(View.GONE);
+            etSearch.setVisibility(View.GONE);
+        } else {
+            empty.setVisibility(View.GONE);
+            scrollView.setVisibility(View.VISIBLE);
+            etSearch.setVisibility(View.VISIBLE);
+
+            if (todayList.isEmpty()) {
+                todayContainer.setVisibility(View.GONE);
+            } else {
+                todayContainer.setVisibility(View.VISIBLE);
+                loadListView(taskListToday, todayList);
+            }
+
+            if (tomorrowList.isEmpty()) {
+                tomorrowContainer.setVisibility(View.GONE);
+            } else {
+                tomorrowContainer.setVisibility(View.VISIBLE);
+                loadListView(taskListTomorrow, tomorrowList);
+            }
+
+            if (upcomingList.isEmpty()) {
+                otherContainer.setVisibility(View.GONE);
+            } else {
+                otherContainer.setVisibility(View.VISIBLE);
+                loadListView(taskListUpcoming, upcomingList);
+            }
+
+            if (todayList.isEmpty() && tomorrowList.isEmpty() && upcomingList.isEmpty())
+                tvNoResults.setVisibility(View.VISIBLE);
+            else tvNoResults.setVisibility(View.GONE);
+        }
+    }
+
+
+    public void loadDataList(Cursor cursor, ArrayList<HashMap<String, String>> dataList) {
+        if (cursor != null) {
+            cursor.moveToFirst();
+            while (cursor.isAfterLast() == false) {
+
+                HashMap<String, String> mapToday = new HashMap<String, String>();
+                mapToday.put("id", cursor.getString(0).toString());
+                mapToday.put("title", cursor.getString(1).toString());
+                mapToday.put("content", cursor.getString(2).toString());
+                mapToday.put("task_at", cursor.getString(3).toString());
+                mapToday.put("status", cursor.getString(4).toString());
+                dataList.add(mapToday);
+                cursor.moveToNext();
+            }
+        }
+    }
+
+    public void loadListView(NoScrollListView listView, final ArrayList<HashMap<String, String>> dataList) {
+        ListTaskAdapter adapter = new ListTaskAdapter(this, dataList, mydb);
+        listView.setAdapter(adapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> parent, View view,
+                                    int position, long id) {
+                Intent i = new Intent(MainActivity.this, AddModifyTask.class);
+                i.putExtra("isModify", true);
+                i.putExtra("id", dataList.get(+position).get("id"));
+                startActivity(i);
+            }
+        });
+    }
+}
